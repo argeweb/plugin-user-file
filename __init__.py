196598887c9d9dd7aed0f3cf86d65ed747ba1503
cwen0708 <cwen0708@gmail.com>
2016-10-23 11:26:00 +0800

    init

---
+++ __init__.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Created with YooLiang Technology (侑良科技).
+# Author: Qi-Liang Wen (温啓良）
+# Web: http://www.yooliang.com/
+# Date: 2015/7/12.
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.ext import blobstore
+import urllib
+
+
+user_file_action_helper = {
+    "group": u"檔案管理",
+    "actions": [
+        {"action": "list", "name": u"檔案管理"},
+        {"action": "images_list", "name": u"圖片列表"},
+        {"action": "add", "name": u"新增檔案管理"},
+        {"action": "edit", "name": u"編輯檔案管理"},
+        {"action": "view", "name": u"檢視檔案管理"},
+        {"action": "delete", "name": u"刪除檔案管理"},
+    ]
+}
+
+class GetFileHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, source_blob_key):
+        if self.request.headers.get('If-None-Match'):
+                return self.error(304)
+
+        blob_key = source_blob_key.split(".")[0]
+        ext = source_blob_key.split(".")[1]
+        if ext in ["jpg", "jpeg", "png", "gif"]:
+            self.response.headers["Content-Transfer-Encoding"] = "base64"
+
+        self.response.headers["Cache-Control"] = "public, max-age=604800"
+        self.response.headers["ETag"] = source_blob_key
+
+        #blob_key = str(urllib.unquote(source_blob_key))
+        blob = blobstore.get(blob_key)
+        if blob:
+            self.response.headers['Content-Type'] = blob.content_type
+            self.send_blob(blob, save_as=False)
+        else:
+            self.redirect("/not_found?path=/userfile/" + source_blob_key)
+
+getfile_app = webapp.WSGIApplication([('/userfile/([^/]+)?', GetFileHandler)],debug=False)
+
+
+class DownloadFileHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, source_blob_key):
+        if self.request.headers.get('If-Modified-Since'):
+            self.error(304)
+            return
+        if source_blob_key.find(".jpg"):
+            self.response.headers['Content-Type'] = "image/jpg"
+            self.response.headers["Content-Transfer-Encoding"] = "base64"
+            source_blob_key = source_blob_key.replace(".jpg", "")
+
+        blob_key = str(urllib.unquote(source_blob_key))
+        self.response.headers["Cache-Control"] = "public, max-age=604800"
+        if not blobstore.get(blob_key):
+            self.redirect("/not_found?path=/download/" + source_blob_key)
+        else:
+            self.send_blob(blobstore.BlobInfo.get(blob_key), save_as=True)
+
+download_app = webapp.WSGIApplication([('/download/([^/]+)?', DownloadFileHandler)],debug=False)
---
+++ controllers/user_file.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Created with YooLiang Technology (侑良科技).
+# Author: Qi-Liang Wen (温啓良）
+# Web: http://www.yooliang.com/
+# Date: 2015/7/12.
+
+import argeweb
+from argeweb import scaffold
+from argeweb.components.pagination import Pagination
+from argeweb.components.search import Search
+from argeweb.components.upload import Upload
+from argeweb import route_with, route_menu
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.ext import blobstore
+
+
+def generate_upload_url(success_path):
+    from google.appengine.ext import blobstore
+    from argeweb import settings
+    cloud_storage_bucket = ""
+    if settings.get('upload').get('use_cloud_storage'):
+        cloud_storage_bucket = settings.get('upload', {}).get('bucket')
+    return blobstore.create_upload_url(
+            success_path= success_path,
+            gs_bucket_name=cloud_storage_bucket)
+
+
+class UserFile(argeweb.Controller):
+    class Meta:
+        components = (scaffold.Scaffolding, Upload, Pagination, Search)
+        pagination_limit = 10
+        pagination_actions = ("list", "images_list",)
+        upload_actions = ("add", "add_from_ui")
+        
+    class Scaffold:
+        title = {
+            "list": u"檔案管理",
+            "add": u"新增檔案",
+            "edit": u"編輯檔案",
+            "view": u"檢視檔案",
+        }
+        display_properties_in_list = ("filename", "content_type", "size", "url")
+
+    @route_with('/admin/user_file/get.json')
+    def admin_get_url(self):
+        self.meta.change_view('json')
+        uri = self.params.get_string("uri", 'admin:user_file:add_from_ui')
+        self.context['data'] = {
+            'url': generate_upload_url(self.uri(uri))
+        }
+
+    @route_menu(list_name=u"backend", text=u"圖片", sort=9800, icon="files-o", group=u"檔案管理")
+    @route_with('/admin/user_file/images_list')
+    def admin_images_list(self):
+        self.meta.pagination_limit = 12
+        model = self.meta.Model
+        def photo_factory(self):
+            return model.query(
+                model.content_type.IN(["image/jpeg", "image/jpg", "image/png", "image/gif"])).order(
+                -model.content_type, -model.created, model._key)
+        self.scaffold.query_factory = photo_factory
+        return scaffold.list(self)
+
+    @route_menu(list_name=u"backend", text=u"檔案", sort=9802, icon="files-o", group=u"檔案管理")
+    def admin_list(self):
+        return scaffold.list(self)
+
+    def admin_add(self):
+        def scaffold_after_apply(**kwargs):
+            item = kwargs["item"]
+            controller = kwargs["controller"]
+            blob_key = blobstore.BlobInfo.get(item.file)
+            item.content_type = blob_key.content_type
+            item.filename = blob_key.filename
+            item.hash = blob_key.md5_hash
+            item.size = blob_key.size
+            item.url = "/userfile/" + str(item.file) + "." + item.filename.split(".")[-1]
+            item.put()
+            controller.context["data"] = {
+                "url": item.url,
+                "item": item
+            }
+        self.events.scaffold_after_apply += scaffold_after_apply
+        return scaffold.add(self)
+
+    @route_with('/admin/user_file/add_from_ui')
+    def admin_add_from_ui(self):
+        self.meta.change_view("json")
+        def scaffold_after_apply(**kwargs):
+            item = kwargs["item"]
+            controller = kwargs["controller"]
+            blob_key = blobstore.BlobInfo.get(item.file)
+            item.content_type = blob_key.content_type
+            item.filename = blob_key.filename
+            item.hash = blob_key.md5_hash
+            item.size = blob_key.size
+            item.url = "/userfile/" + str(item.file) + "." + item.filename.split(".")[-1]
+            item.put()
+            controller.context["data"] = {
+                "url": item.url,
+                "item": item
+            }
+        self.events.scaffold_after_apply += scaffold_after_apply
+        return scaffold.add(self)
+
+    def admin_delete(self, key):
+        try:
+            item = self.util.decode_key(key).get()
+            file_key = item.file
+            blobstore.delete(file_key)
+        except:
+            pass
+        return scaffold.delete(self, key)
